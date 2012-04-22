3a9054c7cd6a60cd7a8cd878ee468b987542acb4
Kjartan Horpestad <kjartan.horpestad@hotmail.com>
2012-04-22 22:39:30 +0200

    oppdater sql classe

--- sqlData.class.php
+++ sqlData.class.php
@@ -1,20 +1,62 @@
 <?php
-class sqlData implements IdataLager{
-	private $xmlFil;
-	private $xmlData;
+class sqlData// implements IdataLager
+{
 	//php@hinv12
 	//gruppe15
 	//kark.hin.no:3306
 	//åpne connection til mysql server
 	
-	$dbc = mysql_connect('kark.hin.no','gruppe15','php@hinv12');
-	if (!$dbc)
-	{
-		die('Not connected' : . mysql_error());
+	private $dbadr;
+	private $dbname;
+	private $username;
+	private $password;
+	private $connected;
+	private $dbc;
+	
+	function __construct($dbadr = 'kark.hin.no', $dbname = 'gruppe15',
+						 $username = 'gruppe15', $password = 'php@hinv12'){
+		
+		$this->dbadr = $dbadr;
+		$this->dbname = $dbname;
+		$this->username = $username;
+		$this->password = $password;
+		$this->connected = false;
+	}
+	
+	public function connect(){
+		$this->$dbc = mysql_connect($dbadr,$username,$password);
+		if (!$dbc){
+			$this->error('Could not connect');
+		}
+		mysql_select_db($dbname,$dbc)
+		or $this->error("Could not select gruppe15");
+		
+		$this->connected = true;
 	}
 
-	function __construct($xmlFil = "innlegg.xml"){
-		$this->xmlFil = $xmlFil;
-		$this->xmlData = simplexml_load_file($this->xmlFil);
+	public function query($query){
+		$this->connect();
+		$result = mysql_query($sql) or $this->error("Query failed");
+		mysql_close();
 	}
+	private function error($error){
+		die($error . mysql_error());
+	}
+	
+	public function test(){
+		$sql = 'SELECT * ';
+		$sql .= 'FROM `Brukere` ';
+		$sql .= 'ORDER BY `id` DESC LIMIT 0, 5';
+		
+		$result = $this->query($sql);
+		
+		while ($row = mysql_fetch_array($result)) {
+			echo "ID:".$row{'id'}." Name:".$row{'brukernavn'}."
+				".$row{'passord'}."<br>";
+				}
+			}
 }
+
+$dbc = new sqlData();
+$dbc->test();
+
