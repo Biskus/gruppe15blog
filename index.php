09d702d47a09728592c27b83fcc3670d2a906696
Kjartan Horpestad <kjartan.horpestad@hotmail.com>
2012-04-24 01:39:16 +0200

    fungerer index

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
-require 'libs/Smarty.class.php';
-require 'class/xmlData.class.php';
-require 'class/innlegg.class.php';
+require_once 'libs/Smarty.class.php';
+require_once 'class/sqlQuery.class.php';
+require_once 'class/innlegg.class.php';
 
 session_start();
 $_SESSION = array();
  $smarty = new Smarty();
- $dataHenter = new xmlData();
+ $dataHenter = new SqlQuery();
  
  $smarty->assign("sideNavn", "Hjem");
- $smarty->assign("innlegg", $dataHenter->hentAlleInnlegg());
+ $smarty->assign("innlegg", $dataHenter->postByDate());
  
  $smarty->display("index.tpl");
