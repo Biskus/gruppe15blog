54fd8dae9735237553565457e4c92e46a3b1e606
Kjartan Horpestad <kjartan.horpestad@hotmail.com>
2012-04-25 21:04:42 +0200

    ting for å få kommentarer til å vises

--- class/innlegg.class.php
+++ class/innlegg.class.php
@@ -7,12 +7,19 @@ class Innlegg{
 	private $autor;
 	private $bruker;
 	private $visninger;
+	private $kommentarer;
 	
-	function __construct($tittel, $tekst, $tagger, $bruker, $visninger, $dato = null){
+	function __construct($tittel, $tekst, $tagger, $bruker, $visninger, $kommentarer, $dato = null){
 		$this->tittel = strip_tags($tittel);
 		$this->tekst = strip_tags($tekst);
 		$this->bruker = strip_tags($bruker);
 		$this->visninger = strip_tags($visninger);
+		
+		if (!empty($kommentarer))
+			$this->kommentarer = $kommentarer;
+		else{
+			$this->kommentarer = null;
+		}
 		$this->tagger = array();
 		foreach ($tagger as $tagg)
 			$this->tagger[] = strip_tags($tagg);		
@@ -23,10 +30,11 @@ class Innlegg{
 			$this->dato = $dato;
 	}
 	
-	public function hentTittel()    {return $this->tittel;} 
-    public function hentTekst()     {return $this->tekst;} 
-    public function hentTagger()    {return $this->tagger;} 
-    public function hentDato()      {return $this->dato;} 
-    public function hentBrukerId()  {return $this->bruker;}
-    public function hentVisninger() {return $this->visninger;}              
+	public function hentTittel()     {return $this->tittel;} 
+    public function hentTekst()      {return $this->tekst;} 
+    public function hentTagger()     {return $this->tagger;} 
+    public function hentDato()       {return $this->dato;} 
+    public function hentBrukerId()   {return $this->bruker;}
+    public function hentVisninger()  {return $this->visninger;} 
+    public function hentKommentarer(){return $this->kommentarer;}             
 }
--- templates_c/7ca6d211b88dfa6ac0d3768fcbd378a515b13f6d.file.hendelser.tpl.php
+++ templates_c/7ca6d211b88dfa6ac0d3768fcbd378a515b13f6d.file.hendelser.tpl.php
@@ -1,23 +1,24 @@
-<?php /* Smarty version Smarty-3.0.6, created on 2012-03-08 00:15:53
+<?php /* Smarty version Smarty-3.0.6, created on 2012-04-25 18:57:04
          compiled from ".\templates\hendelser.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:197064f57fa3917e651-00906760%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:188914f9849003a0291-98590534%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     '7ca6d211b88dfa6ac0d3768fcbd378a515b13f6d' => 
     array (
       0 => '.\\templates\\hendelser.tpl',
-      1 => 1331165750,
+      1 => 1335380216,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '197064f57fa3917e651-00906760',
+  'nocache_hash' => '188914f9849003a0291-98590534',
   'function' => 
   array (
   ),
   'has_nocache_code' => false,
 )); /*/%%SmartyHeaderCode%%*/?>
-<div id="innleggListe">
+<?php if (!is_callable('smarty_function_counter')) include 'C:\Users\kjartan\git\gruppe15blog3\libs\plugins\function.counter.php';
+?><div id="innleggListe">
 	<?php  $_smarty_tpl->tpl_vars['inn'] = new Smarty_Variable;
  $_from = $_smarty_tpl->getVariable('innlegg')->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 if ($_smarty_tpl->_count($_from) > 0){
@@ -39,6 +40,33 @@ if ($_smarty_tpl->_count($_from) > 0){
 </li>
 		<?php }} ?>
 		</ul>
+		<div class="kommentarer">
+			<?php  $_smarty_tpl->tpl_vars['kommentar'] = new Smarty_Variable;
+ $_from = $_smarty_tpl->getVariable('inn')->value->hentKommentarer(); if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+if ($_smarty_tpl->_count($_from) > 0){
+    foreach ($_from as $_smarty_tpl->tpl_vars['kommentar']->key => $_smarty_tpl->tpl_vars['kommentar']->value){
+?>
+			<div id="<?php echo smarty_function_counter(array(),$_smarty_tpl);?>
+">
+				<div class="kommentar_Brukernavn">
+					<?php echo $_smarty_tpl->tpl_vars['kommentar']->value['brukernavn'];?>
+
+				</div>
+				<div class="kommeentar_teks">
+					<?php echo $_smarty_tpl->tpl_vars['kommentar']->value['tekst'];?>
+
+				</div>
+				<div class="kommentar_dato>
+					<?php echo $_smarty_tpl->tpl_vars['kommentar']->value['dato'];?>
+
+				</div>
+				<div class="kommentar_id">
+					<?php echo $_smarty_tpl->tpl_vars['kommentar']->value['id'];?>
+
+				</div>
+			</div>			
+			<?php }} ?>
+		</div>
 	</div>
 	<?php }} ?>
 </div>
