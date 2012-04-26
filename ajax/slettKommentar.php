0f12a028761b6fd0390625429822f9f7354273fc
Arvid Johansen <biskus@gmail.com>
2012-04-26 22:22:10 +0200

    slettkommentar og slettpost

--- ajax/slettKommentar.php
+++ ajax/slettKommentar.php
@@ -4,4 +4,5 @@ require_once ('../class/sqlQuery.class.php');
 $kommentarId = $_GET['postId'];
 $dst = new SqlQuery();
 $result = $dst->slettKommentarByKommentarid($kommentarId);
+//test
 ?>
--- ajax/slettPost.php
+++ ajax/slettPost.php
@@ -4,4 +4,5 @@ require_once ('../class/sqlQuery.class.php');
 $postId = $_GET['postId'];
 $dst = new SqlQuery();
 $result = $dst->slettPostByPostid($postId);
+//test
 ?>
